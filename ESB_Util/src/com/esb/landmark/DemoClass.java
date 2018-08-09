ec7dfa0f770060cd70ef620bdcf498da23535e75
Amit <akd964@gmail.com>
2018-08-09 20:38:07 +0530

    First Commit to master

---
+++ ESB_Util/src/com/esb/landmark/DemoClass.java
@@ -0,0 +1,17 @@
+package com.esb.landmark;
+
+public class DemoClass {
+
+	/**
+	 * Sample method that can be called from a Mapping Custom Java transform.
+	 * The content of this method provides the implementation for the Custom Java transform.
+	 */
+	public static String sampleTransform(String input2) {
+		
+		String output2="";
+		output2=input2.concat("done in java");
+		
+		return output2;
+	}
+
+}
